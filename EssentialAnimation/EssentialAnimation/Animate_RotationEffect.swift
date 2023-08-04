06a19ce62c40335b347ff5ad293d33b74b5b1c50
Rahul Thengadi <thengadir@gmail.com>
2023-08-04 23:15:20 +0530

    Animate RotationEffect

--- EssentialAnimation/EssentialAnimation/Animate_RotationEffect.swift
+++ EssentialAnimation/EssentialAnimation/Animate_RotationEffect.swift
@@ -5,4 +5,35 @@
 //  Created by rahul thengadi on 04/08/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct Animate_RotationEffect: View {
+    @State private var change = false
+
+    var body: some View {
+        VStack {
+            
+            Button("Change") {
+                change.toggle()
+            }
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 300)
+                .overlay(Image(systemName: "arrow.right").foregroundColor(.black))
+                .rotationEffect(Angle.degrees(change ? 220 : 0))
+                .animation(.default, value: change)
+            
+            Spacer()
+        }
+        .font(.title)
+    }
+}
+
+struct Animate_RotationEffect_Previews: PreviewProvider {
+    static var previews: some View {
+        Animate_RotationEffect()
+    }
+}
